0e6b451826f2cef74d8637d91461ef309db63e65
Gautam <whoisgautxm@gmail.com>
2024-12-10 23:08:55 +0530

    Risc0-ZkVm for verifying Attestation

---
+++ host/src/main.rs
@@ -0,0 +1,123 @@
+mod helper;
+mod structs;
+mod zk_proof;
+
+use anyhow::Result;
+use ethers_core::types::H160;
+use helper::domain_separator;
+use methods::ADDRESS_ID;
+use std::fs;
+use std::time::Instant;
+use structs::{Attest, InputData};
+use zk_proof::prove_address;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let start_time = Instant::now();
+
+    // Initialize tracing
+    tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::filter::EnvFilter::from_default_env())
+        .init();
+
+    // Read and parse the JSON file
+    let json_str = fs::read_to_string("./host/src/input.json")?;
+    let input_data: InputData = serde_json::from_str(&json_str)?;
+
+    // Extract data from the parsed JSON
+    let domain = ethers_core::types::transaction::eip712::EIP712Domain {
+        name: Some(input_data.sig.domain.name),
+        version: Some(input_data.sig.domain.version),
+        chain_id: Some(ethers_core::types::U256::from_dec_str(
+            &input_data.sig.domain.chain_id,
+        )?),
+        verifying_contract: Some(input_data.sig.domain.verifying_contract.parse()?),
+        salt: None,
+    };
+
+    let signer_address: H160 = input_data.signer.parse()?;
+
+    let message = Attest {
+        version: input_data.sig.message.version,
+        schema: input_data.sig.message.schema.parse()?,
+        recipient: input_data.sig.message.recipient.parse()?,
+        time: input_data.sig.message.time.parse()?,
+        expiration_time: input_data.sig.message.expiration_time.parse()?,
+        revocable: input_data.sig.message.revocable,
+        ref_uid: input_data.sig.message.ref_uid.parse()?,
+        data: ethers_core::utils::hex::decode(&input_data.sig.message.data[2..])?,
+        salt: input_data.sig.message.salt.parse()?,
+    };
+
+    // Calculate the current timestamp and the threshold age
+    let current_timestamp = chrono::Utc::now().timestamp() as u64;
+    let threshold_age: u64 = 18 * 365 * 24 * 60 * 60; // 18 years in seconds
+
+    // Calculate the domain separator and the message hash
+    let domain_separator = domain_separator(
+        &domain,
+        ethers_core::utils::keccak256(
+            b"EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)",
+        )
+        .into(),
+    );
+
+    // Parse the signature
+    let signature = ethers_core::types::Signature {
+        r: input_data.sig.signature.r.parse()?,
+        s: input_data.sig.signature.s.parse()?,
+        v: input_data.sig.signature.v.into(),
+    };
+
+    // Fields which are sent to the guest code
+    let receipt = prove_address(
+        &signer_address,
+        &signature,
+        &threshold_age,
+        &current_timestamp,
+        &message,         // Pass the entire Attest struct
+        domain_separator, // Pass the domain separator
+    );
+
+    fs::write("receipt.json", serde_json::to_string(&receipt)?)?;
+
+ 
+    let receipt_inner_bytes_array = bincode::serialize(&receipt.inner).unwrap();
+    println!( "Serialized bytes array (hex) INNER: {:?}\n", hex::encode(&receipt_inner_bytes_array));
+
+    fs::write("inner.bin", receipt_inner_bytes_array)?;
+    
+    let receipt_journal_bytes_array = bincode::serialize(&receipt.journal).unwrap();
+    println!( "Serialized bytes array (hex) JOURNAL: {:?}\n", hex::encode(&receipt_journal_bytes_array));
+    fs::write("journal.json", receipt_journal_bytes_array)?;
+
+
+    let mut image_id_hex = String::new();
+    for &value in &ADDRESS_ID {
+    image_id_hex.push_str(&format!("{:08x}", value.to_be()));}
+    println!("Serialized bytes array (hex) IMAGE_ID: {:?}\n", image_id_hex);
+    fs::write("image_id.bin", image_id_hex)?;
+
+    let output: String = receipt.journal.decode().unwrap();
+    println!("Output is: {}", output);
+
+    receipt.verify(ADDRESS_ID).unwrap();
+    println!("Receipt verified.");
+
+    let journal = receipt.journal.bytes.clone();
+    println!("journal:{:?}", journal);
+
+    println!("Proven with guest ID: {}", guest_id());
+
+    let elapsed_time = start_time.elapsed();
+    println!("Execution time: {:?}", elapsed_time);
+
+    Ok(())
+}
+
+fn guest_id() -> String {
+    hex::encode(vec_u8_from_u32_slice_little_endian(&ADDRESS_ID))
+}
+
+fn vec_u8_from_u32_slice_little_endian(v: &[u32]) -> Vec<u8> {
+    v.iter().flat_map(|&x| x.to_le_bytes().to_vec()).collect()
+}
---
+++ host/src/zk_proof.rs
@@ -0,0 +1,37 @@
+// src/zk_proof.rs
+// use crate::structs:: DateOfBirth;
+use crate::structs::Attest;
+use ethers_core::types::Signature;
+use ethers_core::types::{H160, H256};
+use methods::ADDRESS_ELF;
+use risc0_zkvm::{default_prover, ExecutorEnv, Receipt};
+
+
+pub fn prove_address(
+    signer_address: &H160,
+    signature: &Signature,
+    threshold_age: &u64,
+    current_timestamp: &u64,
+    attest: &Attest,
+    domain_separator: H256,
+) -> Receipt {
+    let input: (&H160, &Signature, &u64, &u64, &Attest, H256) = (
+        signer_address,
+        signature,
+        threshold_age,
+        current_timestamp,
+        attest,
+        domain_separator,
+    );
+
+    let env = ExecutorEnv::builder()
+        .write(&input)
+        .unwrap()
+        .build()
+        .unwrap();
+
+    let prover = default_prover();
+    // Read the ELF file into a byte vector
+    
+    prover.prove(env, &ADDRESS_ELF).unwrap().receipt // Pass the byte slice
+}
